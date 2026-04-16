80e389f61c5d006ebc6f558b43fd7898905f2d7b
ilyuha <mopsorar@gmail.com>
2026-04-16 21:00:38 +0500

    ))

--- git/src/main.py
+++ git/src/main.py
@@ -14,7 +14,7 @@ def load_books(filename='library.json'):
         except json.JSONDecodeError:
             return []
 
-def save_books(books, filename='library.json'):
+def saving_books(books, filename='library.json'):
     """
     Сохранение списка книг в JSON-файл.
     """
@@ -66,7 +66,7 @@ def search_books(books, keyword):
 
 def main():
     """
-    Точка входа в программу: здесь мы загружаем книги, 
+    Точка входа в программу: здесь мы загружаем книги,
     показываем меню и обрабатываем ввод пользователя.
     """
     books = load_books()  # Загрузили список книг из JSON
@@ -94,7 +94,7 @@ def main():
             # Получаем новый список с добавленной книгой
             new_books = add_book(books, title, author, year)
             books = new_books  # Обновляем переменную, чтобы сохранить изменения
-            save_books(books)  # Сразу сохраняем в файл
+            saving_books(books)  # Сразу сохраняем в файл
             print("Книга добавлена!")
 
         elif choice == '3':
@@ -102,9 +102,9 @@ def main():
             title_to_remove = input("Введите название книги, которую хотите удалить: ").strip()
 
             new_books = remove_book(books, title_to_remove)
-            if len(new_books) < len(books):
+            if len(new_books) > len(books):
                 books = new_books
-                save_books(books)
+                saving_books(books)
                 print("Книга удалена!")
             else:
                 print("Книга с таким названием не найдена.")
@@ -119,12 +119,17 @@ def main():
             else:
                 print("Ничего не найдено.")
 
-        elif choice == '5':
+        elif choice == '6':
             print("Выход из программы.")
             break
 
         else:
             print("Некорректный ввод. Попробуйте ещё раз.")
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
